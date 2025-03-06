0080c8e4566bd0bcbc1acca2f3f618df3d36bc7d
musabdube <musabdube@gmail.com>
2025-03-06 19:05:12 +0300

    corrected index file

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import IconLink from './IconLink';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { info } from '../../info/info';
 
 function PortfolioBlock(props) {
   const { live, source, title } = props;
   return (
-    <Box
-      display={'flex'}
-      flexDirection={'column'}
-      justifyContent={'center'}
-      alignItems={'center'}
-    >
-      {/* <Box component={'img'} src={image} alt={'mockup'}/> */}
+    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
       <h1 style={{ fontSize: '2rem' }}>{title}</h1>
       <Box
-        className={'portfolio'}
-        display={'flex'}
-        flexDirection={'column'}
-        gap={'0.5rem'}
-        alignItems={'center'}
-        fontSize={'1.5rem'}
-        py={'2rem'}
+        className="portfolio"
+        display="flex"
+        flexDirection="column"
+        gap="0.5rem"
+        alignItems="center"
+        fontSize="1.5rem"
+        py="2rem"
       >
-        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-          <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
-        </Box>
-        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-          <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
-        </Box>
+        {live && (
+          <Box p={1} border="2px solid black" borderRadius="25px">
+            <IconLink link={live} title="Live Demo" icon="fa fa-safari" />
+          </Box>
+        )}
+        {source && (
+          <Box p={1} border="2px solid black" borderRadius="25px">
+            <IconLink link={source} title="Source Code" icon="fa fa-code" />
+          </Box>
+        )}
       </Box>
+      {/* CV Download Button */}
+      <Button
+        variant="contained"
+        color="primary"
+        href={info.cv}
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{ mt: 2 }}
+      >
+        Download My CV
+      </Button>
     </Box>
   );
 }
